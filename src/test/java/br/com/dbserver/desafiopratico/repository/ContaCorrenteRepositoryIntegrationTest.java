23f80c37b65836e6d30c8b83d5223bd90fe13fb1
betaum <robertoramos.net@hotmail.com>
2020-01-10 17:29:08 -0300

    refactory e testes repository (integração)

---
+++ src/test/java/br/com/dbserver/desafiopratico/repository/ContaCorrenteRepositoryIntegrationTest.java
@@ -0,0 +1,35 @@
+package br.com.dbserver.desafiopratico.repository;
+
+import br.com.dbserver.desafiopratico.model.ContaCorrente;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@Sql(value="classpath:sql/limpa_base.sql",
+        executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(value="classpath:sql/insere_dados_base.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+public class ContaCorrenteRepositoryIntegrationTest {
+
+    @Autowired
+    private ContaCorrenteRepository contaCorrenteRepository;
+
+    @Test
+    public void deveriaBuscarPorCodigosDaAgenciaEConta(){
+        Optional<ContaCorrente> contaCorrente =
+                this.contaCorrenteRepository.buscarPorCodigosDaAgenciaEConta(1052L,101020L);
+
+        Assertions.assertEquals(101020L, contaCorrente.get().getConta());
+
+    }
+
+
+}
---
+++ src/test/resources/sql/insere_dados_base.sql
@@ -0,0 +1,1 @@
+insert into tb_conta (cd_conta, cd_agencia, nm_correntista, vl_saldo)  values  (101020, 1052, 'Fulano da Silva', 0.00);
